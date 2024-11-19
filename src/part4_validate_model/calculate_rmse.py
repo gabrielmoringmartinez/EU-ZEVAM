1b55a189a8e5b19c4548acfbe438599598cef037
Gabriel Moring Martinez <gabriel.moeringmartinez@dlr.de>
2024-11-19 16:48:34 +0100

    Other error analysis parameters are removed

--- src/part4_validate_model/calculate_rmse.py
+++ src/part4_validate_model/calculate_rmse.py
@@ -20,28 +20,19 @@ def calculate_rmse(df, config):
                 sum_squared_errors = filtered_country_df['squared_error'].sum()
                 average_error = abs(filtered_country_df['error']).mean()
                 rmse = np.sqrt((1 / m) * sum_squared_errors)
-                average_share = filtered_country_df['share'].mean()
-                n_rmse = rmse/average_share
-                standard_deviation = filtered_country_df['error'].std()
-                max_error = abs(filtered_country_df['error']).max()
-                append_values(rows_rmse, country, timeframe, powertrain, 'real values', rmse, n_rmse, average_error, standard_deviation,
-                              max_error)
+                append_values(rows_rmse, country, timeframe, powertrain, 'real values', rmse)
 
     rmse_df = pd.DataFrame(rows_rmse)
     title = config['title']
     rmse_df.to_csv(f'outputs/rmse_{title}.csv', sep=';', index=False, decimal=',')
     return
 
-def append_values(rows, country, timeframe, powertrain, i, rmse, n_rmse, average_error, standard_deviation, max_error):
+def append_values(rows, country, timeframe, powertrain, i, rmse):
     rows.append({
         'Country': country,
         'Timeframe': timeframe,
         'Powertrain': powertrain,
         'Scenario': i,
         'RMSE': rmse,
-        'Normalized RMSE': n_rmse,
-        'Average Absolute Error': average_error,
-        'Standard Deviation': standard_deviation,
-        'Max Error': max_error
     })
     return rows
