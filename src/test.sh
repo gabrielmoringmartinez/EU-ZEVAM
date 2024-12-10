4be3dd1c7c422838bbc1a6ce61422fc072162723
Gabriel Moring Martinez <gabriel.moeringmartinez@dlr.de>
2024-12-10 14:06:23 +0100

    Add test job to pipeline

---
+++ src/test.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Exit when any command fails
+set -e
+
+# Install required dependencies
+pip install -r stock_model_requirements.txt
+echo "Successfully installed required packages"
+
+# Check the code using the flake8 linter
+flake8 --max-line-length 120 astronaut-analysis.py
+
+# Check that the script is basically working and creating the same results
+python model_european_passenger_car_stock.py
+test -f outputs/figures/CSP 2021_all_countries.pdf
+test -f outputs/figures/battery_electric_vehicle_stock_shares_eu_27_and_norway_up_to_2050_model_reference_scenario_.pdf
+test -f outputs/figures/validation_step_1_actual_new_bev_registrations_and_empirical_csp_curves_all_countries.pdf
+test -f outputs/figures/validation_step_2_actual_new_bev_registrations_and_empirical_csp_curves_all_countries.pdf
+test -f outputs/figures/battery_electric_vehicle_stock_shares_eu_27_and_norway_up_to_2050_with_modified_country_csps_.pdf
+test -f outputs/figures/battery_electric_vehicle_stock_shares_eu_27_and_norway_up_to_2050_with_modified_historical_country_csps_.pdf
+test -f outputs/figures/battery_electric_vehicle_stock_shares_eu_27_and_norway_up_to_2050_with_increased_decreased_country_csps_.pdf
+test -f outputs/figures/battery_electric_vehicle_stock_shares_eu_27_and_norway_up_to_2050_with_modified_registrations_.pdf
+echo "Successfully created the plots"
