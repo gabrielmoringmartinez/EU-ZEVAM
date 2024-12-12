8bf6c06d56788eb71a6f33bc1eb70ff4e3a14fdb
Gabriel Moring Martinez <gabriel.moeringmartinez@dlr.de>
2024-12-12 12:10:56 +0100

    test.sh modified

--- test.sh
+++ test.sh
@@ -7,9 +7,18 @@ set -e
 pip install -r stock_model_requirements.txt
 echo "Successfully installed required packages"
 
+# Run tests
+#pytest model_european_passenger_car_stock_test.py --junitxml=pytest.xml
+#echo "Successfully ran tests"
+
+# Check the code using the ruff linter
+#ruff check --select ALL --ignore PTH,T,PLR,ANN,D205 --output-file=ruff.json --output-format=gitlab model_european_passenger_car_stock.py
+#echo "Successfully ran ruff checks"
+# Check that copyright and license information for all files is available
+
 # Check that the script is basically working and creating the same results
 python model_european_passenger_car_stock.py
-test -f "outputs/figures/CSP 2021_all_countries.pdf"
+#test -f "outputs/figures/CSP 2021_all_countries.pdf"
 test -f "outputs/figures/battery_electric_vehicle_stock_shares_eu_27_and_norway_up_to_2050_model_reference_scenario_.pdf"
 test -f "outputs/figures/validation_step_1_actual_new_bev_registrations_and_empirical_csp_curves_all_countries.pdf"
 test -f "outputs/figures/validation_step_2_actual_new_bev_registrations_and_empirical_csp_curves_all_countries.pdf"
@@ -19,12 +28,4 @@ test -f "outputs/figures/battery_electric_vehicle_stock_shares_eu_27_and_norway_
 test -f "outputs/figures/battery_electric_vehicle_stock_shares_eu_27_and_norway_up_to_2050_with_modified_registrations_.pdf"
 echo "Successfully created the plots"
 
-# Run tests
-#pytest model_european_passenger_car_stock_test.py --junitxml=pytest.xml
-#echo "Successfully ran tests"
-
-# Check the code using the ruff linter
-#ruff check --select ALL --ignore PTH,T,PLR,ANN,D205 --output-file=ruff.json --output-format=gitlab model_european_passenger_car_stock.py
-#echo "Successfully ran ruff checks"
-# Check that copyright and license information for all files is available
 reuse lint
